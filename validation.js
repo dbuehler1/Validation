795be6c5601854fa5137301607e51c6686ca22a9
Daniel <dbuehler1@my.wctc.edu>
2020-11-11 15:44:56 -0600

    Initial commit

---
+++ validation.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    $("registrationForm").submit(runMyProgram);
+    // Object containing the validation rules
+    var myRules =
+        {
+            firstName:
+                {
+                    required: true
+                },
+            grade:
+                {
+                    required: true
+                }
+
+        };
+
+    // Object containing the error messages
+    var myMessages =
+        {
+            firstName: "Please Enter Your Name",
+            grade: "Enter Your Age"
+        };
+
+    // Pass the configuration to the form's validate() method
+    // Needs submitHandler, rules, and messages properties
+    $("form").validate(
+        {
+            submitHandler: runMyProgram,
+            rules: myRules,
+            messages: myMessages
+        }
+    );
+
+    function runMyProgram() {
+
+        var name = $("#firstName").val();
+        var gradeA = $("#grade").val();
+        // Change the text of the <p> with ID of "message" to
+        // state, for example, "You have registered Jane for grade 6 camp!"
+        // Use the name and grade the user provided in the form.
+        $("#message").text("You have registered " + name + " for grade " + gradeA + " camp!");
+
+
+    }
+
+
+
+});
